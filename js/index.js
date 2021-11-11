4cd5ec2191e87d9ae9a9ef0396e641ddd536f390
jay <jaydesigns2020@gmail.com>
2021-11-11 00:40:38 -0800

    added boolean includes checks for values in the expression

--- js/index.js
+++ js/index.js
@@ -6,8 +6,43 @@ $(document).ready(function(){
     $('#main_button').click(function(){
 
         val = $('#main_input').val();
-    
-        alert(val);
+
+        val = val.toLowerCase();
+
+        valArray = val.split(" ");
+
+        console.log(valArray)
+        isValid = checkValid(valArray)
+
+        console.log('Valid Expression?: ' + isValid)
+
+        //TO DO check to see if string is valid
+
+        isP = val.includes('p');
+        isQ = val.includes('q');
+        isR = val.includes('r');
+        
+        
+        isNot = val.includes('!');
+        isAnd = val.includes('&&');
+        isOr = val.includes('||');
+        isImply = val.includes('->');
+        isBi = val.includes('<=>');
+
+        
+        console.log('p : ' + isP + ', q: ' + isQ + ', r: ' + isR)
+        console.log('Not: ' + isNot + ', And: ' + isAnd + ', Or: ' + isOr + ', Implies: ' + isImply + ', Bi-Conditional: ' + isBi)
+
+        // TO DO search for each case in in the string
+
+        // TO DO break each string into parts to analyze
+
+        // TO DO see if each part is true or false
+
+        // TO DO combine different parts to make whole expression
+
+        // TO DO display result and truth table
+        
     });
 
     
@@ -15,3 +50,26 @@ $(document).ready(function(){
 });
 
 
+function checkValid(val){
+
+    console.log('Value Array: ' + val);
+
+    console.log('Expression Length: ' + val.length)
+
+    terms = ['p', 'q', 'r', '!', '&&', '||', '->', '<=>']
+
+    for(i=0; i<val.length; i++){
+
+        if(!terms.includes(val[i])){
+
+            return false;
+                
+        }
+
+        
+    }
+    
+    return true;
+
+
+}
