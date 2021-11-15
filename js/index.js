2f8e4960d136dd975c7d55cb9cb13688600183ca
jay <jaydesigns2020@gmail.com>
2021-11-15 02:05:55 -0800

    separating the expression...

--- js/index.js
+++ js/index.js
@@ -7,6 +7,9 @@ $(document).ready(function(){
 
         val = $('#main_input').val();
 
+
+        //check to see if string is valid
+
         val = val.toLowerCase();
 
         valArray = val.split(" ");
@@ -16,7 +19,9 @@ $(document).ready(function(){
 
         console.log('Valid Expression?: ' + isValid)
 
-        //TO DO check to see if string is valid
+       
+
+        //search for each case in the string
 
         isP = val.includes('p');
         isQ = val.includes('q');
@@ -33,9 +38,41 @@ $(document).ready(function(){
         console.log('p : ' + isP + ', q: ' + isQ + ', r: ' + isR)
         console.log('Not: ' + isNot + ', And: ' + isAnd + ', Or: ' + isOr + ', Implies: ' + isImply + ', Bi-Conditional: ' + isBi)
 
-        // TO DO search for each case in in the string
+        //get all positions in the string
 
+        const position = JSON.stringify(checkPos(valArray))
+
+        posJson  = JSON.parse(position)
+
+        console.log("All Positions: " + posJson)
+
+
+
+        //get main vairables (p,q,r)
+
+        const mainVar = JSON.stringify(checkVar(valArray))
+
+        varJson = JSON.parse(mainVar)
+
+        console.log("Main Variable Positions: " + varJson)
+
+
+
+        //get operators in string (!, &&, ||, ->, <=>)
+
+        const operators = JSON.stringify(checkOps(valArray))
+
+        opsJson = JSON.parse(operators)
+
+        console.log("Operator Positions: " + opsJson)
+        
+
+
+
+        
         // TO DO break each string into parts to analyze
+
+        // TO DO loop through eachn case for every part of the string
 
         // TO DO see if each part is true or false
 
@@ -50,6 +87,8 @@ $(document).ready(function(){
 });
 
 
+
+//checks to see if all characters in expression are valid
 function checkValid(val){
 
     console.log('Value Array: ' + val);
@@ -58,7 +97,7 @@ function checkValid(val){
 
     terms = ['p', 'q', 'r', '!', '&&', '||', '->', '<=>']
 
-    for(i=0; i<val.length; i++){
+    for(let i=0; i<val.length; i++){
 
         if(!terms.includes(val[i])){
 
@@ -73,3 +112,73 @@ function checkValid(val){
 
 
 }
+
+
+//finds the position of each term in the expression
+function checkPos(val){
+
+    terms = ['p', 'q', 'r', '!', '&&', '||', '->', '<=>']
+
+    posArr = []
+
+    for(let i=0; i<val.length; i++){
+
+        for(let j=0; j<terms.length; j++)
+            if (terms[j] == val[i]){
+
+                posArr.push(i + " : " + terms[j])
+            }
+
+    }
+
+
+    return posArr 
+
+
+
+}
+
+
+function checkVar(val){
+
+    terms = ['p', 'q', 'r']
+
+    posArr = []
+
+    for(let i=0; i<val.length; i++){
+
+        for(let j=0; j<terms.length; j++)
+            if (terms[j] == val[i]){
+
+                posArr.push(i + " : " + terms[j])
+            }
+
+    }
+
+
+    return posArr 
+
+}
+
+
+function checkOps(val){
+
+    terms = ['!', '&&', '||', '->', '<=>']
+
+    posArr = []
+    
+    for(let i=0; i<val.length; i++){
+
+        for(let j=0; j<terms.length; j++)
+            if (terms[j] == val[i]){
+
+                posArr.push(i + " : " + terms[j])
+            }
+
+    }
+
+
+    return posArr 
+
+}
+
